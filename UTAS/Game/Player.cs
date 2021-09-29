61ed3329eee590407bec0e5bc4d567e8b78b8299
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2021-09-29 11:51:25 -0400

    Added some more sprites, and a very basic background.

--- UTAS/Game/Player.cs
+++ UTAS/Game/Player.cs
@@ -8,10 +8,17 @@ namespace UTAS
 {
     class Player
     {
-        private Animation leftAnim;
-        private Animation rightAnim;
-        private Animation upAnim;
-        private Animation downAnim;
-        private Animation idleAnim;
+        //private Animation leftAnim;
+        //private Animation rightAnim;
+        //private Animation upAnim;
+       // private Animation downAnim;
+     //   private Animation idleAnim;
+        
+        
+        
+        public void LoadContent()
+        {
+         //   rightAnim = new Animation(Load<Texture2D>("Sprites/Asriel/spr_asrielkid_right"), 0.25f, true);
+        }
     }
 }
--- UTAS/Game1.cs
+++ UTAS/Game1.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Content;
 
 namespace UTAS
 {
@@ -8,19 +14,22 @@ namespace UTAS
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        public KeyboardState currentKeyboardState;
 
+        Texture2D home;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-
+        // https://en.wikipedia.org/wiki/List_of_common_resolutions
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
             Window.Title = "Undertale: Asriel's Story";
-
+            _graphics.PreferredBackBufferHeight = 768;
+            _graphics.PreferredBackBufferWidth = 1280;
+            _graphics.ApplyChanges();
             base.Initialize();
         }
 
@@ -29,13 +38,26 @@ namespace UTAS
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+
+           home =  Content.Load<Texture2D>("Rooms\\New Home\\bg_HomeMainEnt");
         }
 
         protected override void Update(GameTime gameTime)
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
                 Exit();
+            }
+
+            currentKeyboardState = Keyboard.GetState();
+
+            if (currentKeyboardState.IsKeyDown(Keys.A) || currentKeyboardState.IsKeyDown(Keys.Left))
+            {
+                Debug.WriteLine("Moving Left");
+            }
+            
+            
 
             // TODO: Add your update logic here
 
@@ -45,7 +67,10 @@ namespace UTAS
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(home, new Rectangle(_graphics.PreferredBackBufferWidth / 4, _graphics.PreferredBackBufferHeight /  4, 
+                home.Width * 2, home.Height * 2), Color.White);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
