7acf9ef009fc4f7979e3498985df750f7393defb
Aubrey <51178662+FoxinTale@users.noreply.github.com>
2022-02-07 20:47:11 -0500

    Started work on animating the player sprite.

--- UTAS/Game/Player.cs
+++ UTAS/Game/Player.cs
@@ -1,5 +1,6 @@
 using System;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 // https://www.dreamincode.net/forums/topic/194878-xna-animated-sprite/
 
@@ -8,45 +9,137 @@ namespace UTAS
     class Player : Sprite
     {
         private Vector2 velocity = new Vector2(0, 0);
+        public Vector2 playerPosition;
         private float speed = 4;
+        public float health = 20;
+        public Texture2D playerSpritesheet;
+        public Rectangle[,] playerSpriteCuts;
         public Player(string texSource) : base(texSource){}
         public bool canMove = true;
-        private bool direction;
-        Point currentSprite;
+        private bool notIdle = true;
+        public bool direction;
+        public Point currentSprite;
+        int priorX;
+        double lastAnimate;
 
-        public void Initialize()
+        public void Initialize(GraphicsDeviceManager _graphics)
         { 
             direction = true;
             currentSprite = new Point(0, 0);
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth/2 - 9, _graphics.PreferredBackBufferHeight/2 - 14);
+            playerSpriteCuts = new Rectangle[,] { 
+                {
+                    new Rectangle(0, 0, 18, 28), 
+                    new Rectangle(18, 0, 18, 28), 
+                    new Rectangle( 36, 0, 18, 28), 
+                    new Rectangle( 54, 0, 18, 28) 
+                }, {
+                    new Rectangle(0, 28, 18, 28), 
+                    new Rectangle(18, 28, 18, 28),
+                    new Rectangle(-1, -1, 0, 0),
+                    new Rectangle(-1, -1, 0, 0)
+                }, {
+                    new Rectangle(0, 56, 18, 28), 
+                    new Rectangle(18, 56, 18, 28),
+                    new Rectangle(-1, -1, 0, 0),
+                    new Rectangle(-1, -1, 0, 0)
+                },{
+                    new Rectangle(0, 84, 18, 28), 
+                    new Rectangle(18,  84, 18, 28), 
+                    new Rectangle( 36, 84, 18, 28), 
+                    new Rectangle( 54, 84,  18, 28)
+                },
+            };
+
         }
         public override void Update(GameTime gt)
         {
+            KeyboardState kstate = Keyboard.GetState();
+
             if (canMove)
             {
-                KeyboardState kstate = Keyboard.GetState();
                 if (kstate.IsKeyDown(Keys.Up) || kstate.IsKeyDown(Keys.W) || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y > 0)
                 {
-                    velocity.Y = -speed;
+                   // velocity.Y = -speed;
+                   currentSprite.X = 0;
+                  // cameraPosition.Y += 2.5f;
+                   notIdle = true;
                 }
                 else if (kstate.IsKeyDown(Keys.Down) || kstate.IsKeyDown(Keys.S) || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y < 0)
                 {
-                    velocity.Y = speed;
+                   // velocity.Y = speed;
+                   currentSprite.X = 0;
+               //    cameraPosition.Y -= 2.5f;
+                   notIdle = true;
                 }
 
                 if (kstate.IsKeyDown(Keys.Left) || kstate.IsKeyDown(Keys.A) || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X < 0)
                 {
-                    velocity.X = - speed;
+                  //  velocity.X = - speed;
+                  currentSprite.X = 0;
+                  // cameraPosition.X += 2.5f;
+                  direction = true;
+                  notIdle = true;
                 }
                 else if (kstate.IsKeyDown(Keys.Right) || kstate.IsKeyDown(Keys.D) || GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X > 0)
                 {
-                    velocity.X = speed;
+                   // velocity.X = speed;
+                   currentSprite.X = 0;
+                 //  cameraPosition.X -= 2.5f;
+                   direction = false;
+                   notIdle = true;
+                }
+                if (!notIdle)
+                {
+                    currentSprite.X = 4;
+                }
+                if (priorX != currentSprite.X)
+                {
+                    currentSprite.Y = 0;
+                    lastAnimate = gt.TotalGameTime.TotalMilliseconds;
+                }
+                else if (gt.TotalGameTime.TotalMilliseconds - lastAnimate > 60)
+                {
+                    if (currentSprite.Y < 9 && playerSpriteCuts[currentSprite.X, currentSprite.Y].X != -1)
+                        currentSprite.Y += 1;
+                    else
+                        currentSprite.Y = 0;
+                    if (currentSprite.X == 2 && currentSprite.Y == 4)
+                    {
+
+                        if (Keyboard.GetState().IsKeyDown(Keys.E) || GamePad.GetState(PlayerIndex.One).Buttons.LeftShoulder == ButtonState.Pressed)
+                        {
+                            currentSprite.Y = 0;
+                            currentSprite.X = 3;
+                        }
+                        else
+                        {
+                            currentSprite.Y = 0;
+                            currentSprite.X = 4;
+                            canMove = false;
+                        }
+                    }
+                    if (currentSprite.X == 3 && currentSprite.Y == 5)
+                    {
+                        currentSprite.Y = 0;
+                        currentSprite.X = 4;
+                        canMove = false;
+                    }
+                    lastAnimate = gt.TotalGameTime.TotalMilliseconds;
                 }
 
-                pos += velocity;
-                velocity = Vector2.Zero;
+                priorX = currentSprite.X;
+
+               pos += velocity;
+               // velocity = Vector2.Zero;
 
             }
-          
+
+            if (kstate.IsKeyDown(Keys.L))
+            {
+                health = 0;
+            }
         }
+        
     }
 }
--- UTAS/MainGame.cs
+++ UTAS/MainGame.cs
@@ -7,31 +7,75 @@ namespace UTAS
 {
     public class MainGame : Game
     {
-        private GraphicsDeviceManager _graphics;
+        public GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private Song homeTheme;
+        private Song deathTheme;
 
         Texture2D home;
-        private Player asriel;
-   
+
+        // private Player asriel;
+        private Vector2 velocity = new Vector2(0, 0);
+        private Vector2 pos;
+        public Vector2 playerPosition;
+        private float speed = 4;
+        public float health = 20;
+        public Texture2D playerSpritesheet;
+        public Rectangle[,] playerSpriteCuts;
+        public bool canMove = true;
+        private bool notIdle = false;
+        public bool direction;
+        public Point currentSprite;
+        int priorX;
+        double lastAnimate;
+
         public MainGame()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
-        
+
         // https://en.wikipedia.org/wiki/List_of_common_resolutions
         protected override void Initialize()
         {
             Window.Title = "Undertale: Asriel's Story";
             this.Content.RootDirectory = "Content";
-            
-            _graphics.PreferredBackBufferHeight = 768; // 768
-            _graphics.PreferredBackBufferWidth = 1280; // 1280
+
+            _graphics.PreferredBackBufferHeight = 768;
+            _graphics.PreferredBackBufferWidth = 1280;
             _graphics.ApplyChanges();
-            asriel = new Player("Sprites\\Characters\\Asriel\\spr_asrielkid_idle");
+            //    asriel = new Player("Sprites\\Characters\\Asriel\\spr_asrielkid_idle");
             base.Initialize();
-            asriel.Initialize();
+            //      asriel.Initialize(_graphics);
+            direction = true;
+            currentSprite = new Point(0, 0);
+            pos = new Vector2(_graphics.PreferredBackBufferWidth / 2 - 9, _graphics.PreferredBackBufferHeight / 2 - 14);
+            playerPosition = new Vector2(_graphics.PreferredBackBufferWidth/2 - 9, _graphics.PreferredBackBufferHeight/2 - 14);
+            playerSpriteCuts = new Rectangle[,] { 
+                {
+                    new Rectangle(0, 0, 18, 28), 
+                    new Rectangle(18, 0, 18, 28), 
+                    new Rectangle( 36, 0, 18, 28), 
+                    new Rectangle( 54, 0, 18, 28) 
+                }, {
+                    new Rectangle(0, 28, 18, 28), 
+                    new Rectangle(18, 28, 18, 28),
+                    new Rectangle(-1, -1, 0, 0),
+                    new Rectangle(-1, -1, 0, 0)
+                }, {
+                    new Rectangle(0, 56, 18, 28), 
+                    new Rectangle(18, 56, 18, 28),
+                    new Rectangle(-1, -1, 0, 0),
+                    new Rectangle(-1, -1, 0, 0)
+                },{
+                    new Rectangle(0, 84, 18, 28), 
+                    new Rectangle(18,  84, 18, 28), 
+                    new Rectangle( 36, 84, 18, 28), 
+                    new Rectangle( 54, 84,  18, 28)
+                },
+            };
+
         }
 
         protected override void LoadContent()
@@ -40,15 +84,20 @@ namespace UTAS
 
             // TODO: use this.Content to load your game content here
 
-           home =  Content.Load<Texture2D>("Rooms\\New Home\\bg_HomeMainEnt");
-            asriel.LoadContent(Content);
-
+            home = Content.Load<Texture2D>("Rooms\\New Home\\bg_HomeMainEnt");
+            // asriel.LoadContent(Content);
+            playerSpritesheet = Content.Load<Texture2D>("Sprites\\Characters\\Asriel\\spr_asrielkid");
+            homeTheme = Content.Load<Song>("Music/mus_home_remix");
+            deathTheme = Content.Load<Song>("Music/mus_death");
             try
             {
                 MediaPlayer.IsRepeating = true;
-                MediaPlayer.Play(Content.Load<Song>("Music/mus_home_remix"));
-            } catch {}
-
+                MediaPlayer.Play(homeTheme);
+            }
+            catch
+            {
+                // empty because for whatever reason it is required.
+            }
         }
 
         protected override void Update(GameTime gameTime)
@@ -60,9 +109,88 @@ namespace UTAS
             }
 
 
-            asriel.Update(gameTime);
+            KeyboardState kstate = Keyboard.GetState();
 
-        //    InputHandler(gameTime);
+           // if (canMove)
+         //   {
+         notIdle = false;
+         if (kstate.IsKeyDown(Keys.Up) || kstate.IsKeyDown(Keys.W) ||
+             GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y > 0)
+                {
+                    velocity.Y = -speed;
+                    currentSprite.Y = 0;
+                    notIdle = true;
+                }
+                else if (kstate.IsKeyDown(Keys.Down) || kstate.IsKeyDown(Keys.S) ||
+                         GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y < 0)
+                {
+                    velocity.Y = speed;
+                    currentSprite.Y = 0;
+                    notIdle = true;
+                }
+
+                if (kstate.IsKeyDown(Keys.Left) || kstate.IsKeyDown(Keys.A) ||
+                    GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X < 0)
+                {
+                    velocity.X = - speed;
+                    currentSprite.X = 0;
+                    direction = true;
+                    notIdle = true;
+                }
+                else if (kstate.IsKeyDown(Keys.Right) || kstate.IsKeyDown(Keys.D) ||
+                         GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.X > 0)
+                {
+                    velocity.X = speed;
+                    currentSprite.X = 0;
+                    direction = false;
+                    notIdle = true;
+                }
+
+                if (!notIdle)
+                {
+                    currentSprite.X = 1;
+                }
+
+                if (priorX != currentSprite.X)
+                {
+                    currentSprite.Y = 0;
+                    lastAnimate = gameTime.TotalGameTime.TotalMilliseconds;
+                }
+                else if (gameTime.TotalGameTime.TotalMilliseconds - lastAnimate > 60)
+                {
+                    if (currentSprite.Y < 3 && playerSpriteCuts[currentSprite.X, currentSprite.Y].X != -1)
+                        currentSprite.Y += 1;
+                    else
+                        currentSprite.Y = 0;
+                    if (currentSprite.X == 2 && currentSprite.Y == 3)
+                    {
+                        if (Keyboard.GetState().IsKeyDown(Keys.E) ||
+                            GamePad.GetState(PlayerIndex.One).Buttons.LeftShoulder == ButtonState.Pressed)
+                        {
+                            currentSprite.Y = 0;
+                            currentSprite.X = 3;
+                        }
+                        else
+                        {
+                            currentSprite.Y = 0;
+                            currentSprite.X = 3;
+                         //   canMove = false;
+                        }
+                    }
+
+                    if (currentSprite.X == 3 && currentSprite.Y == 3)
+                    {
+                        currentSprite.Y = 0;
+                        currentSprite.X = 3;
+                      //  canMove = false;
+                    }
+                    lastAnimate = gameTime.TotalGameTime.TotalMilliseconds;
+                }
+
+                priorX = currentSprite.X;
+                pos += velocity;
+                velocity = Vector2.Zero;
+          //  }
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -71,12 +199,16 @@ namespace UTAS
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.Black);
-            
+
             _spriteBatch.Begin();
-            _spriteBatch.Draw(home, new Rectangle(_graphics.PreferredBackBufferWidth / 4, _graphics.PreferredBackBufferHeight /  4, 
-                home.Width * 2, home.Height * 2), Color.White); 
-            
-            asriel.draw(_spriteBatch);
+            _spriteBatch.Draw(home, new Rectangle(_graphics.PreferredBackBufferWidth / 4,
+                _graphics.PreferredBackBufferHeight / 4,
+                home.Width * 2, home.Height * 2), Color.White);
+
+            //   asriel.draw(_spriteBatch);
+           // _spriteBatch.Draw(playerSpritesheet, );
+            _spriteBatch.Draw(playerSpritesheet, pos, playerSpriteCuts[currentSprite.X, currentSprite.Y], Color.White, 
+                0f, new Vector2(0), 2, SpriteEffects.None, 1);
             _spriteBatch.End();
             base.Draw(gameTime);
         }
